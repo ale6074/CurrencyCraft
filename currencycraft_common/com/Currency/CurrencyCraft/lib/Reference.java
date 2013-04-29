5414599e705e667527afb7bbe4e0dae600a57c3f
blinkshot300 <alexandervasil840@gmail.com>
2013-04-29 19:01:25 -0400

    Inital commit of LGPL,GPL, and CurrencyCraft Files

---
+++ currencycraft_common/com/Currency/CurrencyCraft/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.Currency.CurrencyCraft.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "Currency";
+    public static final String MOD_NAME = "CurrencyCraft";
+    public static final String VERSION = "0.0.1";
+}
